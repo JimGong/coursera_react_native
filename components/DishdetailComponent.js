49cee7de317062d4d55713247eff45ac9e0dd0f1
JiaMin Gong <magicbear730@gmail.com>
2020-08-29 17:03:00 -0700

    persist redux store

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -22,6 +22,7 @@ const mapDispatchToProps = dispatch => ({
 function RenderDish(props) {
 	const dish = props.dish;
 
+	// handleViewRef = ref => this.view = ref;
 
 	const recognizeDrag = ({moveX, moveY, dx, dy}) => {
 		if (dx < -200)
@@ -32,6 +33,9 @@ function RenderDish(props) {
 	const panResponder = PanResponder.create({
 		onStartShouldSetPanResponder: (e, gestureState) => {
 			return true;
+		},
+		onPanResponderGrant: () => {
+			// this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : "cancelled"));
 		},
 		onPanResponderEnd: (e, gestureState) => {
 			console.log("pan responder end", gestureState);
@@ -56,7 +60,9 @@ function RenderDish(props) {
 
 	if (dish) {
 		return (
-				<Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
+				<Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+				                 {...panResponder.panHandlers}
+				>
 					<Card
 							featuredTitle={dish.name}
 							image={{uri: baseUrl + dish.image}}>
